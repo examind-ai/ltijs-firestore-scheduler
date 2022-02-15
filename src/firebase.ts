63adc091e844c000947a3e232637be89146f3f53
Johnny Oshika <johnnyoshika@gmail.com>
2022-02-15 10:57:17 -0800

    Configure firebase

---
+++ src/firebase.ts
@@ -0,0 +1,9 @@
+import * as admin from 'firebase-admin';
+
+// initializeApp looks for service account credentials in GOOGLE_APPLICATION_CREDENTIALS environment variable
+// and determines which Firebase project we're using
+admin.initializeApp();
+
+const db = admin.firestore();
+
+export { db };
